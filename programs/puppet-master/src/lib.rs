2ec7a78f43f314d5d43c8089f588f65d975a7023
Raj Gedhada <grajnikanth@gmail.com>
2022-10-26 10:46:47 -0700

    set_data_ctx function implemented on PullStrings struct - refactored code

--- programs/puppet-master/src/lib.rs
+++ programs/puppet-master/src/lib.rs
@@ -16,40 +16,18 @@ pub mod puppet_master {
     use super::*;
 
 
-    // prepare the Context for the puppet program in this function
-    // Using the Context prepared here, we can then call the puppet program
-    // via cross program invocation from this function to update the 
-    // data in the Data struct
-
-    // pull_strings will call the set_data function of the puppet program
-    // To do CPI in anchor, the Conext shall include the accounts SetData 
-    // validation struct required + the puppet program account to do CPI
-
-    // client has to send in the publickey of the puppet program. which
-    // will be validated by the PullStrings struct
 
 
-    // anchor Context accounts field is Deserialized accounts. So looks like
-    // when sending this accounts to the puppet program via cpi they have
-    // to be converted to AccountInfo structs
+// cpi conext preparation is handles in the set_data_ctx function implemented
+// on the PullStrings struct below
 
-    // cpi_program variable is set to the account_info struct by converting the
-    // Program struct to account_info using to_account_info() function
-    // This function was implemented on the 
-    // anchor_lang::accounts::program::Program struct
+// once we get the CpiContext we can now call the puppet set_data function
+// from the puppet_master program
 
-    // The cpi invocation is syntax heavy in the sense that you have to follow
-    // the anchor syntax for cpi invocation
+// The set_data function returns Ok(())
 
     pub fn pull_strings(ctx: Context<PullStrings>, data: u64) -> Result<()> {
-        
-        let cpi_program = ctx.accounts.puppet_program.to_account_info();    
-        let cpi_accounts = SetData {
-            puppet: ctx.accounts.puppet_data.to_account_info(),
-        };
-
-        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-        puppet::cpi::set_data(cpi_ctx, data);
+        puppet::cpi::set_data(ctx.accounts.set_data_ctx(), data)
     }
 }
 
@@ -71,4 +49,47 @@ pub struct PullStrings<'info> {
     #[account(mut)]
     pub puppet_data: Account<'info, Data>,
     pub puppet_program: Program<'info, Puppet>
+}
+
+// implement the set_data_ctx function as a function on the PullStrings struct
+// to prepare the cpi call to puppet program. This will keep the business logic 
+// in pull_strings function separate from cpi preparation logic
+
+// self here is the PullStrings struct. The ctx.accounts sent to the 
+// instruction by client is also same as PullStrings struct
+
+// prepare the Context for the puppet program in this function
+// Using the Context prepared here, we can then call the puppet program
+// via cross program invocation from this function to update the 
+// data in the Data struct
+
+// pull_strings will call the set_data function of the puppet program
+// To do CPI in anchor, the Conext shall include the accounts SetData 
+// validation struct required + the puppet program account to do CPI
+
+// client has to send in the publickey of the puppet program. which
+// will be validated by the PullStrings struct
+
+
+// anchor Context accounts field is Deserialized accounts. So looks like
+// when sending this accounts to the puppet program via cpi they have
+// to be converted to AccountInfo structs
+
+// cpi_program variable is set to the account_info struct by converting the
+// Program struct to account_info using to_account_info() function
+// This function was implemented on the 
+// anchor_lang::accounts::program::Program struct
+
+// The cpi invocation is syntax heavy in the sense that you have to follow
+// the anchor syntax for cpi invocation
+
+impl<'info> PullStrings<'info> {
+    pub fn set_data_ctx(&self) -> CpiContext<'_, '_, '_, 'info, SetData<'info>> {
+        let cpi_program = self.puppet_program.to_account_info();    
+        let cpi_accounts = SetData {
+            puppet: self.puppet_data.to_account_info(),
+        };
+
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
 }
