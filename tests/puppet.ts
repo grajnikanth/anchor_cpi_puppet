1e91bc50778060a1a726c56ffbc21d2a5c3a3318
Raj Gedhada <grajnikanth@gmail.com>
2022-10-26 11:31:30 -0700

    Implemented the cpi test

--- tests/puppet.ts
+++ tests/puppet.ts
@@ -1,16 +1,48 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { Puppet } from "../target/types/puppet";
+import { Keypair } from '@solana/web3.js';
+import { expect } from 'chai';
+import { PuppetMaster } from '../traget/types/puppet_master';
 
 describe("puppet", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.Puppet as Program<Puppet>;
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const puppetProgram = anchor.workspace.Puppet as Program<Puppet>;
+  const puppetMasterProgram = anchor.workspace.PuppetMaster as Program<PuppetMaster>;
+
+  // puppetKeypair will be the address where a new account will be created
+  // using puppet program
+  const puppetKeypair = Keypair.generate();
+
+  it("Does CPI", async () => {
+
+    await puppetProgram.methods
+        .initialize()
+        .accounts({
+          puppet: puppetKeypair.publicKey,
+          user: provider.wallet.publicKey
+        })
+        .signers([puppetKeypair])
+        .rpc();
+    
+    await puppetMasterProgram.methods
+          .pullStrings(new anchor.BN(15))
+          .accounts({
+            puppetProgram: puppetProgram.programId,
+            puppetData: puppetKeypair.publicKey
+          })
+          .rpc();
+    
+    let puppetDataAccount = await puppetProgram.account.data.fetch(
+        puppetKeypair.publicKey);
+
+    expect(puppetDataAccount.data.toNumber()).to.equal(15);
+
   });
+    
+  
+
 });
